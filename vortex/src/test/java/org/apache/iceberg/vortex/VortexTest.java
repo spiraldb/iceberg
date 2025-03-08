0b4b6c3def4fb03df4e53ffa60c1b0f827096ed7
Andrew Duffy <andrew@a10y.dev>
2025-03-08 12:44:29 -0500

    Generic reader JUnit test

--- vortex/src/test/java/org/apache/iceberg/vortex/VortexTest.java
+++ vortex/src/test/java/org/apache/iceberg/vortex/VortexTest.java
@@ -18,17 +18,99 @@
  */
 package org.apache.iceberg.vortex;
 
+import static org.apache.iceberg.types.Types.NestedField.required;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Files;
 import org.apache.iceberg.Schema;
+import org.apache.iceberg.data.GenericRecord;
+import org.apache.iceberg.data.Record;
+import org.apache.iceberg.data.vortex.GenericVortexReader;
+import org.apache.iceberg.io.CloseableIterable;
+import org.apache.iceberg.io.InputFile;
+import org.apache.iceberg.io.SeekableInputStream;
+import org.apache.iceberg.relocated.com.google.common.collect.ImmutableList;
+import org.apache.iceberg.relocated.com.google.common.io.ByteStreams;
 import org.apache.iceberg.types.Types;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
 
 public final class VortexTest {
-  // Create schema for TPC-H nation table.
-  private static final Schema SCHEMA =
-      new Schema(Types.NestedField.required(1, "n_nationkey", Types.IntegerType.get()));
+  private static final Schema EMPLOYEE_SCHEMA =
+      new Schema(
+          required(1, "id", Types.LongType.get()),
+          required(2, "name", Types.StringType.get()),
+          required(3, "salary", Types.LongType.get()));
+
+  @TempDir private static File TEMP_DIR;
 
   @Test
-  public void testBasic() {
-    //        Vortex.read()
+  public void testGenericReader() throws IOException {
+    InputFile inputFile = loadResourceFile("employees.vortex", TEMP_DIR);
+
+    try (CloseableIterable<Record> records =
+        Vortex.read(inputFile)
+            .project(EMPLOYEE_SCHEMA)
+            .readerFunction(GenericVortexReader::buildReader)
+            .build()) {
+
+      assertEquals(
+          ImmutableList.of(
+              makeEmployee(1L, "Alice", 1_000),
+              makeEmployee(2L, "Bob", 2_000),
+              makeEmployee(3L, "Carol", 3_000)),
+          ImmutableList.copyOf(records));
+    }
+  }
+
+  private static InputFile loadResourceFile(String location, File outDir) {
+    outDir.mkdirs();
+
+    var outPath = outDir.toPath().resolve(location);
+
+    try (InputStream inputStream = VortexTest.class.getResourceAsStream(location);
+        OutputStream outputStream = Files.newOutputStream(outPath)) {
+      if (inputStream == null) {
+        throw new IllegalArgumentException("Resource not found: " + location);
+      }
+      ByteStreams.copy(inputStream, outputStream);
+    } catch (IOException e) {
+      throw new RuntimeException("Failed to load resource: " + location, e);
+    }
+
+    return new InputFile() {
+      @Override
+      public long getLength() {
+        return 0;
+      }
+
+      @Override
+      public SeekableInputStream newStream() {
+        return null;
+      }
+
+      @Override
+      public String location() {
+        return outPath.toAbsolutePath().toString();
+      }
+
+      @Override
+      public boolean exists() {
+        return true;
+      }
+    };
+  }
+
+  private static Record makeEmployee(long id, String name, long salary) {
+    GenericRecord record = GenericRecord.create(EMPLOYEE_SCHEMA);
+    record.set(0, id);
+    record.set(1, name);
+    record.set(2, salary);
+
+    return record;
   }
 }
