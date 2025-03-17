75e7485c928346cf1995cca510f3edc0c7e68e96
Andrew Duffy <andrew@a10y.dev>
2025-03-17 18:57:18 -0400

    cloud storage properties

--- vortex/src/main/java/org/apache/iceberg/vortex/VortexIterable.java
+++ vortex/src/main/java/org/apache/iceberg/vortex/VortexIterable.java
@@ -21,11 +21,15 @@ package org.apache.iceberg.vortex;
 import dev.vortex.api.Array;
 import dev.vortex.api.ArrayStream;
 import dev.vortex.api.DType;
+import dev.vortex.api.File;
 import dev.vortex.api.ScanOptions;
-import dev.vortex.impl.NativeFile;
+import dev.vortex.impl.Files;
 import java.io.IOException;
+import java.net.URI;
 import java.util.List;
+import java.util.Map;
 import java.util.function.Function;
+import org.apache.hadoop.conf.Configuration;
 import org.apache.iceberg.Schema;
 import org.apache.iceberg.hadoop.HadoopInputFile;
 import org.apache.iceberg.io.CloseableGroup;
@@ -35,8 +39,12 @@ import org.apache.iceberg.io.InputFile;
 import org.apache.iceberg.relocated.com.google.common.base.Preconditions;
 import org.apache.iceberg.relocated.com.google.common.collect.Lists;
 import org.apache.iceberg.types.Types;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class VortexIterable<T> extends CloseableGroup implements CloseableIterable<T> {
+  private static final Logger LOG = LoggerFactory.getLogger(VortexIterable.class);
+
   private final InputFile inputFile;
   private final Function<DType, VortexRowReader<T>> rowReaderFunc;
   private final Function<DType, VortexBatchReader<T>> batchReaderFunction;
@@ -58,8 +66,7 @@ public class VortexIterable<T> extends CloseableGroup implements CloseableIterab
   @Override
   public CloseableIterator<T> iterator() {
     // TODO(aduffy): this HadoopInputFile assumption is maybe silly.
-    String path = ((HadoopInputFile) inputFile).getPath().toUri().getPath();
-    NativeFile vortexFile = NativeFile.open(path);
+    File vortexFile = newVortexFile(inputFile);
     addCloseable(vortexFile);
 
     ArrayStream batchStream =
@@ -76,6 +83,70 @@ public class VortexIterable<T> extends CloseableGroup implements CloseableIterab
     }
   }
 
+  private static File newVortexFile(InputFile inputFile) {
+    Preconditions.checkArgument(
+        inputFile instanceof HadoopInputFile, "Vortex only supports HadoopInputFile currently");
+
+    HadoopInputFile hadoopInputFile = (HadoopInputFile) inputFile;
+    URI path = hadoopInputFile.getPath().toUri();
+    switch (path.getScheme()) {
+      case "s3a":
+        return Files.open(path, s3PropertiesFromHadoopConf(hadoopInputFile.getConf()));
+      case "file":
+        return Files.open(path, Map.of());
+      default:
+        // TODO(aduffy): add support for Azure
+        throw new IllegalArgumentException("Unsupported scheme: " + path.getScheme());
+    }
+  }
+
+  static final String FS_S3A_ACCESS_KEY = "fs.s3a.access.key";
+  static final String FS_S3A_SECRET_KEY = "fs.s3a.secret.key";
+  static final String FS_S3A_SESSION_TOKEN = "fs.s3a.session.token";
+  static final String FS_S3A_ENDPOINT = "fs.s3a.endpoint";
+  static final String FS_S3A_ENDPOINT_REGION = "fs.s3a.endpoint.region";
+  static final String FS_S3A_CREDENTIALS_PROVIDER = "fs.s3a.aws.credentials.provider";
+  static final String ANONYMOUS_CREDENTIALS_PROVIDER =
+      "org.apache.hadoop.fs.s3a.AnonymousAWSCredentialsProvider";
+
+  private static Map<String, String> s3PropertiesFromHadoopConf(Configuration hadoopConf) {
+    VortexS3Properties properties = new VortexS3Properties();
+
+    for (Map.Entry<String, String> entry : hadoopConf) {
+      switch (entry.getKey()) {
+        case FS_S3A_ACCESS_KEY:
+          properties.setAccessKeyId(entry.getValue());
+          break;
+        case FS_S3A_SECRET_KEY:
+          properties.setSecretAccessKey(entry.getValue());
+          break;
+        case FS_S3A_SESSION_TOKEN:
+          properties.setSessionToken(entry.getValue());
+          break;
+        case FS_S3A_ENDPOINT:
+          properties.setEndpoint(entry.getValue());
+          break;
+        case FS_S3A_ENDPOINT_REGION:
+          properties.setRegion(entry.getValue());
+          break;
+        case FS_S3A_CREDENTIALS_PROVIDER:
+          if (entry.getValue().equals(ANONYMOUS_CREDENTIALS_PROVIDER)) {
+            properties.setSkipSignature(true);
+          } else {
+            throw new IllegalArgumentException(
+                "Vortex does not support custom AWSCredentialsProvider: " + entry.getValue());
+          }
+          break;
+        default:
+          LOG.warn(
+              "Ignoring unknown s3a connector property: {}={}", entry.getKey(), entry.getValue());
+          break;
+      }
+    }
+
+    return properties.asProperties();
+  }
+
   static class VortexBatchIterator implements CloseableIterator<Array> {
     private ArrayStream stream;
 
@@ -85,9 +156,7 @@ public class VortexIterable<T> extends CloseableGroup implements CloseableIterab
 
     @Override
     public Array next() {
-      Array result = stream.getCurrent();
-
-      return result;
+      return stream.getCurrent();
     }
 
     @Override
