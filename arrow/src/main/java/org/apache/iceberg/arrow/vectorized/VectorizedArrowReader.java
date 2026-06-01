773aaf7a27b48a1889a80f68bc70b55851c6d740
Robert Kruszewski <github@robertk.io>
2026-06-01 21:35:14 +0100

    beter

--- arrow/src/main/java/org/apache/iceberg/arrow/vectorized/VectorizedArrowReader.java
+++ arrow/src/main/java/org/apache/iceberg/arrow/vectorized/VectorizedArrowReader.java
@@ -891,10 +891,7 @@ public class VectorizedArrowReader implements VectorizedReader<VectorHolder> {
   private static BigIntVector allocateBigIntVector(Field field, int valueCount) {
     BigIntVector vector = (BigIntVector) field.createVector(ArrowAllocation.rootAllocator());
     vector.allocateNew(valueCount);
-    ArrowBuf validityBuffer = vector.getValidityBuffer();
-    for (int i = 0; i < valueCount; i += 1) {
-      BitVectorHelper.setBit(validityBuffer, i);
-    }
+    vector.getValidityBuffer().setOne(0L, valueCount);
 
     return vector;
   }
