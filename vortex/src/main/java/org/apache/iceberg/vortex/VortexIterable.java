e2c65a836dd7e5b38593108ab7958f3f5faf381a
Robert Kruszewski <github@robertk.io>
2026-06-17 01:43:55 +0100

    format

--- vortex/src/main/java/org/apache/iceberg/vortex/VortexIterable.java
+++ vortex/src/main/java/org/apache/iceberg/vortex/VortexIterable.java
@@ -26,7 +26,6 @@ import dev.vortex.api.ScanOptions;
 import dev.vortex.api.Session;
 import dev.vortex.jni.NativeRuntime;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.NoSuchElementException;
